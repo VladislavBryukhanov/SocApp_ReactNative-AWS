00c33bbd1622f629d4ee507cc793bc2fc5026dfd
nameless <gfedcba625@gmail.com>
2020-04-06 21:49:42 +0300

    fixed db layer auth, replaced sender identity to backend - appsync => authorized messages

--- src/components/ChatInput/chat-input.component.tsx
+++ src/components/ChatInput/chat-input.component.tsx
@@ -5,23 +5,13 @@ import { IconButton } from 'react-native-paper';
 import { useMutation } from 'react-apollo';
 import createMessageMutation from '../../api/graphql/mutations/createMessage.graphql';
 import styles from './styles';
-import { connect } from 'react-redux';
-import { AppState } from '@store/index';
-import { User } from '@models/user';
-
-interface ChatInputProps {
-  profile: User
-}
-
-const ChatInput: React.FC<ChatInputProps> = (props: ChatInputProps) => {
-  const { id, username } = props.profile;
+const ChatInput: React.FC = () => {
   const [message, setMessage] = useState('');
   const [addMessage, { loading }] = useMutation(createMessageMutation, {
     variables: {
-      'createmessageinput': {
-        'chatId': '9xNZWKEix0LrYvTl2IuIKZLWWsHzDMIJ',
-        'senderKey': { id, username },
-        'content': message,
+      createmessageinput: {
+        chatId: '9xNZWKEix0LrYvTl2IuIKZLWWsHzDMIJ',
+        content: message,
       }
     },
     onCompleted: () => setMessage('')
@@ -45,8 +35,4 @@ const ChatInput: React.FC<ChatInputProps> = (props: ChatInputProps) => {
   );
 }
 
-const mapStateToProps = (store: AppState) => ({
-  profile: store.usersModule.profile!
-});
-
-export default connect(mapStateToProps)(ChatInput);
+export default ChatInput;
