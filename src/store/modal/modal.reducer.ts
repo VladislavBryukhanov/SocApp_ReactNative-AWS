f0bbb8881d8fb6088ad5f91d36cf5b9f2708642c
gfedcba625@gmail.com <gfedcba625@gmail.com>
2020-08-13 10:52:34 +0300

    subscription chat filter is required, refactored modal reducer

--- src/store/modal/modal.reducer.ts
+++ src/store/modal/modal.reducer.ts
@@ -1,6 +1,7 @@
 import { Action } from 'redux';
 import { clone } from 'lodash';
 import { OPEN_MODAL, CLOSE_MODAL, OPEN_SUBMODAL } from '@store/action-types';
+import { handleActions } from 'redux-actions';
 
 export interface ModalComponent {
   modalComponent: React.ReactNode
@@ -18,24 +19,16 @@ const initState: ModalState = {
   modalDialogs: []
 }
 
-export const modalReducer = (state = initState, actions: ModalAction): ModalState => {
-  switch(actions.type) {
-    case OPEN_MODAL: 
-      return {
-        modalDialogs: [actions.payload]
-      }
-    case OPEN_SUBMODAL:
-      return {
-        modalDialogs: [
-          ...state.modalDialogs,
-          actions.payload
-        ]
-      };
-    case CLOSE_MODAL:
-      const mutableModalList = clone(state);
-      mutableModalList.modalDialogs.pop();
-      return mutableModalList;
-    default:
-      return state;
+export const modalReducer = handleActions({
+  [OPEN_MODAL]: (state: ModalState, { payload }: ModalAction) => ({
+      modalDialogs: [payload]
+  }),
+  [OPEN_SUBMODAL]: (state: ModalState, { payload }: ModalAction) => ({
+    modalDialogs: [ ...state.modalDialogs, payload ]
+  }),
+  [CLOSE_MODAL]: (state: ModalState) => {
+    const mutableModalList = clone(state);
+    mutableModalList.modalDialogs.pop();
+    return mutableModalList;
   }
-}
+}, initState);
