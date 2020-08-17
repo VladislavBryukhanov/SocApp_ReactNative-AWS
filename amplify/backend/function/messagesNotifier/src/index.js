193e5702db7a722da67be78d406fb055341d698d
gfedcba625@gmail.com <gfedcba625@gmail.com>
2020-08-17 21:29:46 +0300

    updated appSync schema, notification lambda and client app according to new dynamodb schema

--- amplify/backend/function/messagesNotifier/src/index.js
+++ amplify/backend/function/messagesNotifier/src/index.js
@@ -17,41 +17,39 @@ const MESSAGE_RECEIVED_TAG = 'message-received';
 const TopicArn = process.env.SNS_MSG_TOPIC_ARN;
 const TableName = process.env.STORAGE_USERLIST_NAME;
 
-exports.handler = (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+exports.handler = async (event, context, callback) => {
+    const record = event.Records.find(({ eventName }) => eventName === 'INSERT');
 
-    event.Records.forEach(async record => {
-        if (record.eventName == 'INSERT') {
-            const { createdAt, senderKey, content, chatId } = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
-            
-            const { Item: sender } = await dynamodb.get({
-                TableName,
-                Key: senderKey,
-            }).promise();
+    if (!record) return;
 
-            const notificationPayload = {
-                data: {
-                    tag: MESSAGE_RECEIVED_TAG,
-                    message: content,
-                    chatId,
-                    createdAt,
-                    sender
-                },
-                priority: 'high'
-            };
-            
-            const message = JSON.stringify({ 
-                GCM: JSON.stringify(notificationPayload),
-                default: JSON.stringify(notificationPayload)
-            });
-            
-            const params = {
-                MessageStructure: 'json',
-                Message: message,
-                TopicArn,
-            };
-            
-            SNS.publish(params, callback);   
-        }
-    })
+    const { createdAt, senderId, content, chatId } = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
+    
+    const { Item: sender } = await dynamodb.get({
+        TableName,
+        Key: { id: senderId },
+    }).promise();
+
+    const notificationPayload = {
+        data: {
+            tag: MESSAGE_RECEIVED_TAG,
+            message: content,
+            chatId,
+            createdAt,
+            sender
+        },
+        priority: 'high'
+    };
+    
+    const message = JSON.stringify({ 
+        GCM: JSON.stringify(notificationPayload),
+        default: JSON.stringify(notificationPayload)
+    });
+    
+    const params = {
+        MessageStructure: 'json',
+        Message: message,
+        TopicArn,
+    };
+    
+    SNS.publish(params, callback);   
 };
